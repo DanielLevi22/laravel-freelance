598f61e300035fc52081590fc04c15d81fabac91
Daniel Levi <daniellevi000@gmail.com>
2024-10-16 23:34:22 -0300

    feat: add factories proposals

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,13 +21,21 @@ class DatabaseSeeder extends Seeder
             ->create();
 
 
-        User::query()
+            User::query()
             ->inRandomOrder()
             ->limit(10)
             ->get()
             ->each(function (User $user) {
-                Project::factory()->create(['created_by' => $user->id]);
+                // Cria um projeto atribuído ao usuário atual
+                $project = Project::factory()->create(['created_by' => $user->id]);
+
+                // Cria uma quantidade aleatória de propostas (de 1 a 120) para o projeto
+                Proposal::factory()
+                    ->count(random_int(1, 120))
+                    ->create(['project_id' => $project->id]); // Correção aqui
             });
+
+
     }
 
 }
